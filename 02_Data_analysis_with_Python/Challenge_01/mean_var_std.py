125621c7b2a8432f9c79d80b8c4759c8f658ba31
DBWALLER <52055874+DBWALLER@users.noreply.github.com>
2022-11-09 13:17:57 -0300

    Create mean_var_std.py

---
+++ 02_Data_analysis_with_Python/Challenge_01/mean_var_std.py
@@ -0,0 +1,19 @@
+
+
+import numpy as np
+
+def calculate(list):
+    if len(list)!=9:
+        raise ValueError("List must contain nine numbers.")
+    else:
+        a=np.array(list)
+        m=a.reshape ((3,3))
+        dic={}
+        
+        dic['mean'] =  [ m.mean(axis=0).tolist(), m.mean(axis=1).tolist(), m.mean()]
+        dic['variance'] =   [ m.var(axis=0).tolist(), m.var(axis=1).tolist(), m.var()]
+        dic['standard deviation']= [ m.std(axis=0).tolist(), m.std(axis=1).tolist(), m.std()]
+        dic['max'] =  [ m.max(axis=0).tolist(), m.max(axis=1).tolist(), m.max()]
+        dic['min'] =  [ m.min(axis=0).tolist(), m.min(axis=1).tolist(), m.min()]
+        dic['sum'] =   [ m.sum(axis=0).tolist(), m.sum(axis=1).tolist(), m.sum()]
+        return dic
